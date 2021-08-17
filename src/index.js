deecdc9840e7cb37c36c6923094814b27f896362
medaminedev66 <md.amine.smahi@gmail.com>
2021-08-17 16:31:08 +0100

    Add iterateTasks function to create task elements

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import _ from 'lodash';
-import './style.css';
+import _ from "lodash";
+import "./style.css";
 
-const list = [{
-        description: "Complete the to do list project",
-        completed: false,
-        index: 0,
-    },
-    {
-        description: "Complete the to do list project",
-        completed: false,
-        index: 0,
-    }
+const list = [
+  {
+    description: "Complete the to do list project",
+    completed: false,
+    index: 0,
+  },
+  {
+    description: "Learn how to set up webpack",
+    completed: false,
+    index: 0,
+  },
 ];
+
+const iterateTasks = () => {
+  list.forEach(() => {
+    const listContainer = document.createElement("div");
+    const description = document.createElement("p");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    document.querySelector(".todolist").appendChild(listContainer);
+    listContainer.appendChild(checkbox);
+    listContainer.appendChild(description);
+  });
+};
