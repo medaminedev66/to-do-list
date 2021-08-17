1a1db15c41092ff9b919340b7984041f97252767
medaminedev66 <md.amine.smahi@gmail.com>
2021-08-17 16:08:09 +0100

    Set up linters, Add an array of object to index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import _ from 'lodash';
 import './style.css';
+
+const list = [{
+        description: "Complete the to do list project",
+        completed: false,
+        index: 0,
+    },
+    {
+        description: "Complete the to do list project",
+        completed: false,
+        index: 0,
+    }
+];
