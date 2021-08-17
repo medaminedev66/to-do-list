1c53c1974a42842d944368c93e7b87ab16ba8449
medaminedev66 <md.amine.smahi@gmail.com>
2021-08-17 22:56:40 +0100

    Add styles to to-do-list class

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import _ from "lodash";
-import "./style.css";
+import './style.css';
 
 const list = [
   {
-    description: "Complete the to do list project",
+    description: 'Complete the to do list project',
     completed: false,
     index: 2,
   },
   {
-    description: "Learn how to set up webpack",
+    description: 'Learn how to set up webpack',
     completed: false,
     index: 1,
   },
   {
-    description: "Go to bed",
+    description: 'Go to bed',
     completed: false,
     index: 4,
   },
   {
-    description: "check with Ali his process during this week",
+    description: 'check with Ali his process during this week',
     completed: false,
     index: 3,
   },
@@ -26,11 +25,12 @@ const list = [
 
 const iterateTasks = () => {
   list.forEach((task) => {
-    const listContainer = document.createElement("div");
-    const description = document.createElement("p");
-    const checkbox = document.createElement("input");
-    document.querySelector(".todolist").appendChild(listContainer);
-    checkbox.type = "checkbox";
+    const listContainer = document.createElement('div');
+    listContainer.className = 'listContainer';
+    const description = document.createElement('p');
+    const checkbox = document.createElement('input');
+    document.querySelector('.todolist').appendChild(listContainer);
+    checkbox.type = 'checkbox';
     listContainer.appendChild(checkbox);
     listContainer.appendChild(description);
     description.innerText = task.description;
@@ -39,11 +39,11 @@ const iterateTasks = () => {
 
 const arrangeList = () => {
   let max = list[0].index;
-  for (let i = 1; i < list.length; i=+) {
+  for (let i = 1; i < list.length; i += 1) {
     if (list[i].index > max) {
       max = list[i].index;
     } else {
-      let temp = list[i];
+      const temp = list[i];
       list[i] = list[i - 1];
       list[i - 1] = temp;
     }
--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    devServer: {
-        contentBase: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [{
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        }, ],
-    },
+  },
 };
