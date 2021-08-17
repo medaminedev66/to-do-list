b4802fbb5b37b2e1a5a67c436cd7a3e5420c12be
medaminedev66 <md.amine.smahi@gmail.com>
2021-08-17 22:24:18 +0100

    Add renderList function to implement the list

--- src/index.js
+++ src/index.js
@@ -5,23 +5,53 @@ const list = [
   {
     description: "Complete the to do list project",
     completed: false,
-    index: 0,
+    index: 2,
   },
   {
     description: "Learn how to set up webpack",
     completed: false,
-    index: 0,
+    index: 1,
+  },
+  {
+    description: "Go to bed",
+    completed: false,
+    index: 4,
+  },
+  {
+    description: "check with Ali his process during this week",
+    completed: false,
+    index: 3,
   },
 ];
 
 const iterateTasks = () => {
-  list.forEach(() => {
+  list.forEach((task) => {
     const listContainer = document.createElement("div");
     const description = document.createElement("p");
     const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
     document.querySelector(".todolist").appendChild(listContainer);
+    checkbox.type = "checkbox";
     listContainer.appendChild(checkbox);
     listContainer.appendChild(description);
+    description.innerText = task.description;
   });
 };
+
+const arrangeList = () => {
+  let max = list[0].index;
+  for (let i = 1; i < list.length; i=+) {
+    if (list[i].index > max) {
+      max = list[i].index;
+    } else {
+      let temp = list[i];
+      list[i] = list[i - 1];
+      list[i - 1] = temp;
+    }
+  }
+};
+
+const renderList = () => {
+  arrangeList();
+  iterateTasks();
+};
+renderList();
